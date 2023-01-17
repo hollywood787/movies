b96ade46d21f39b5cbb4acb25aed2c89286bc523
hollywood787 <hollywood7878@yandex.ru>
2023-01-17 18:12:06 +0300

    add layout app

---
+++ src/components/moviesList/movie/movie.tsx
@@ -0,0 +1,58 @@
+import "./movie.css";
+
+export default function Movie() {
+  return (
+    <div className="movie__block">
+      <div className="movie__block-image">
+        <img src="./assets/images/time.jpg" alt="" />
+      </div>
+      <div className="movie__block-data">
+        <div className="movie__block-data-information">
+          <div className="movie__block-data-information-rating">
+            Рейтинг 8.4
+          </div>
+          <div className="movie__block-data-information-star">
+            <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+              <rect fill="none" height="256" width="256" />
+              <path
+                d="M128,216S28,160,28,92A52,52,0,0,1,128,72h0A52,52,0,0,1,228,92C228,160,128,216,128,216Z"
+                opacity="0.2"
+              />
+              <path
+                d="M128,216S28,160,28,92A52,52,0,0,1,128,72h0A52,52,0,0,1,228,92C228,160,128,216,128,216Z"
+                fill="#fff"
+                stroke="#000"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="16"
+              />
+            </svg>
+          </div>
+          <div className="movie__block-data-information-star">
+            <svg viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+              <rect fill="#fff" height="256" width="256" />
+              <path
+                d="M168,224l-56-40L56,224V72a8,8,0,0,1,8-8h96a8,8,0,0,1,8,8Z"
+                opacity="0.2" fill="#fff"
+              />
+              <path
+                d="M168,224l-56-40L56,224V72a8,8,0,0,1,8-8h96a8,8,0,0,1,8,8Z"
+                fill="none"
+                stroke="#000"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="16"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="movie__block-data-title">
+          Истребитель демонов: Поезд "Бесконечный"
+        </div>
+        <a className="about_movie" href="/">
+          Подробнее
+        </a>
+      </div>
+    </div>
+  );
+}
