8990e33e0c41a1bf7604f2d019ecba2b1d708504
hollywood787 <hollywood7878@yandex.ru>
2023-01-19 21:17:21 +0300

    add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import Header from "./components/header/header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MoviesList from "./components/moviesList/moviesList";
 import Filters from "./components/filters/filters";
+import React, { useState } from "react";
+
+export const CurrentPageContext = React.createContext();
+export const ChangeCurrentPageContext = React.createContext();
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(0);
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -15,8 +21,12 @@ function App() {
       <section id="hero">
         <div className="container-fluid">
           <div className="hero__block">
-            <Filters />
-            <MoviesList />
+            <CurrentPageContext.Provider value={currentPage}>
+              <ChangeCurrentPageContext.Provider value={setCurrentPage}>
+                <Filters />
+                <MoviesList />
+              </ChangeCurrentPageContext.Provider>
+            </CurrentPageContext.Provider>
           </div>
         </div>
       </section>
--- src/components/moviesList/moviesList.tsx
+++ src/components/moviesList/moviesList.tsx
@@ -1,15 +1,23 @@
 import Movie from "./movie/movie";
 import "./moviesList.css";
+import { listMovies } from "../mocks/listMovies.js";
+import { CurrentPageContext } from "../../App";
+import { useContext } from "react";
+import { countPageSwitch } from "../../consts";
 
 export default function MoviesList() {
+  const currentPage: any = useContext(CurrentPageContext);
+  const result = listMovies.slice(currentPage, currentPage + countPageSwitch);
+
   return (
-      <div className="movie__list-container">
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
+    <div className="movie__list-container">
+      {result.map((item) => (
+        <Movie
+          key={item.id}
+          title={item.title}
+          voteAverage={item.vote_average}
+        />
+      ))}
     </div>
   );
 }
